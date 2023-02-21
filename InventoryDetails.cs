8394af26543b60f44cbe297277c219478dcda5c0
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-21 20:13:49 +0530

    Add : More Data

--- InventoryDetails.cs
+++ InventoryDetails.cs
@@ -9,8 +9,22 @@ namespace InventoryManagement
     public class InventoryDetails
     {
         public List<TypeofRice> typeofRice;
+        public List<TypeofPulses> typeofPulses;
+        public List<Typeofwheat> typeofWheat;
     }
     public class TypeofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypeofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class Typeofwheat
     {
         public string name;
         public int weight;
